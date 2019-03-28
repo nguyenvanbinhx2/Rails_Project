35b69d6135d155952668bfde19048d4aeef96a07
Nguyen Van Binh <songthu.1235@gmail.com>
2019-03-28 14:26:35 +0700

    New commit project

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 # Be sure to restart your server when you modify this file.
+=======
+ # Be sure to restart your server when you modify this file.
+>>>>>>> New commit project
 
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
