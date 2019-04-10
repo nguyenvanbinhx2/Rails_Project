a5167aa526fca79ed0d0e704a442ba58305a6900
Nguyen Van Binh <songthu.1235@gmail.com>
2019-04-10 15:30:00 +0700

    Chapter 7 is ready

--- config/application.rb
+++ config/application.rb
@@ -6,5 +6,8 @@ Bundler.require(*Rails.groups)
 module SampleApp
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :vi]
   end
 end
